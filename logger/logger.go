7ff1811243c5ef469bb1d795270c13c8cb8b14e8
shanebarnes <shanebarnes@users.noreply.github.com>
2017-11-13 23:11:16 -0500

    Switched to variadic functions

--- logger/logger.go
+++ logger/logger.go
@@ -43,7 +43,7 @@ func SetLevel(level Level) {
     atomic.StoreInt32(&loggerLevel, int32(level))
 }
 
-func println(level Level, msg *string) {
+func println(level Level, v ...interface{}) {
     if level >= GetLevel() {
         prefix := ""
 
@@ -58,22 +58,23 @@ func println(level Level, msg *string) {
             prefix = "[ALW]"
         }
 
-        getLogger().Println(prefix, *msg)
+        v = append([]interface{}{prefix}, v...)
+        getLogger().Println(v...)
     }
 }
 
-func PrintlnAlways(msg string) {
-    println(Always, &msg)
+func PrintlnAlways(v ...interface{}) {
+    println(Always, v...)
 }
 
-func PrintlnDebug(msg string) {
-    println(Debug, &msg)
+func PrintlnDebug(v ...interface{}) {
+    println(Debug, v...)
 }
 
-func PrintlnError(msg string) {
-    println(Error, &msg)
+func PrintlnError(v ...interface{}) {
+    println(Error, v...)
 }
 
-func PrintlnInfo(msg string) {
-    println(Info, &msg)
+func PrintlnInfo(v ...interface{}) {
+    println(Info, v...)
 }
