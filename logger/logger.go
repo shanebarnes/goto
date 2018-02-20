6459be21041ac1fd686912f605785e99cfb11887
shanebarnes <shanebarnes@users.noreply.github.com>
2018-02-19 21:30:19 -0500

    Allow function execution time to be incorporated into logger message

--- logger/logger.go
+++ logger/logger.go
@@ -1,105 +1,117 @@
 package logger
 
 import (
-    "io"
-    "log"
-    "sync"
-    "sync/atomic"
+	"io"
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type Level int32
 
 const (
-    Debug Level = iota
-    Info
-    Error
-    Always
+	Debug Level = iota
+	Info
+	Error
+	Always
 )
 
 type loggerObject struct {
-    Flags   int
-    Level   int32
-    Logger *log.Logger
-    Writer  io.Writer
+	Flags  int
+	Level  int32
+	Logger *log.Logger
+	Writer io.Writer
 }
 
 type logger struct {
-    Jam  []loggerObject
-    Once   sync.Once
+	Jam  []loggerObject
+	Once sync.Once
 }
 
 var instance logger
 
 func GetLevel(index int) Level {
-    var ret Level = Always
-    logger := getLogger()
+	var ret Level = Always
+	logger := getLogger()
 
-    if index < len(logger.Jam) {
-        ret = Level(atomic.LoadInt32(&logger.Jam[index].Level))
-    }
+	if index < len(logger.Jam) {
+		ret = Level(atomic.LoadInt32(&logger.Jam[index].Level))
+	}
 
-    return ret
+	return ret
 }
 
 func getLogger() *logger {
-    instance.Once.Do(func() {
-        for i := range instance.Jam {
-            instance.Jam[i].Logger = log.New(instance.Jam[i].Writer,
-                                             "",
-                                             instance.Jam[i].Flags)
-        }
-    })
-    return &instance
+	instance.Once.Do(func() {
+		for i := range instance.Jam {
+			instance.Jam[i].Logger = log.New(instance.Jam[i].Writer,
+				"",
+				instance.Jam[i].Flags)
+		}
+	})
+	return &instance
 }
 
 func Init(flags int, level Level, writer io.Writer) {
-    instance.Jam = append(instance.Jam, loggerObject{ Flags: flags, Level: int32(level), Writer: writer })
+	instance.Jam = append(instance.Jam, loggerObject{Flags: flags, Level: int32(level), Writer: writer})
 }
 
 func SetLevel(index int, level Level) {
-    logger := getLogger()
+	logger := getLogger()
 
-    if index < len(logger.Jam) {
-        atomic.StoreInt32(&logger.Jam[index].Level, int32(level))
-    }
+	if index < len(logger.Jam) {
+		atomic.StoreInt32(&logger.Jam[index].Level, int32(level))
+	}
 }
 
 func println(level Level, v ...interface{}) {
-    logger := getLogger()
+	logger := getLogger()
 
-    for i := range logger.Jam {
-        if level >= GetLevel(i) {
-            prefix := ""
+	for i := range logger.Jam {
+		if level >= GetLevel(i) {
+			prefix := ""
 
-            switch level {
-            case Debug:
-                prefix = "[DBG]"
-            case Info:
-                prefix = "[INF]"
-            case Error:
-                prefix = "[ERR]"
-            case Always:
-                prefix = "[ALW]"
-            }
+			switch level {
+			case Debug:
+				prefix = "[DBG]"
+			case Info:
+				prefix = "[INF]"
+			case Error:
+				prefix = "[ERR]"
+			case Always:
+				prefix = "[ALW]"
+			}
 
-            m := append([]interface{}{prefix}, v...)
-            logger.Jam[i].Logger.Println(m...)
-        }
-    }
+			m := append([]interface{}{prefix}, v...)
+			logger.Jam[i].Logger.Println(m...)
+		}
+	}
 }
 
 func PrintlnAlways(v ...interface{}) {
-    println(Always, v...)
+	println(Always, v...)
 }
 
 func PrintlnDebug(v ...interface{}) {
-    println(Debug, v...)
+	println(Debug, v...)
 }
 
 func PrintlnError(v ...interface{}) {
-    println(Error, v...)
+	println(Error, v...)
 }
 
 func PrintlnInfo(v ...interface{}) {
-    println(Info, v...)
+	println(Info, v...)
+}
+
+func PrintlnTime(l Level, start time.Time, v ...interface{}) func() {
+	if start.IsZero() {
+		start = time.Now()
+	}
+
+	return func() {
+		m := append([]interface{}{"(exec time: " + time.Since(start).String() + ")"}, v...)
+		println(l, m...)
+	}
 }
--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,104 +1,121 @@
 package logger
 
 import (
-    "bytes"
-    "testing"
+	"bytes"
+	"fmt"
+	"testing"
+	"time"
 )
 
 var output *bytes.Buffer = new(bytes.Buffer)
 
 func TestLoggerInit(t *testing.T) {
-    message := "Hello, world!"
-    expected := "[INF] Hello, world!\n"
+	message := "Hello, world!"
+	expected := "[INF] Hello, world!\n"
 
-    Init(0, Info, output)
-    PrintlnInfo(message)
+	Init(0, Info, output)
+	PrintlnInfo(message)
 
-    if output.String() != expected {
-        t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
-    }
+	if output.String() != expected {
+		t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
+	}
 
-    output.Reset()
+	output.Reset()
 }
 
 func TestLoggerSetLevel(t *testing.T) {
-    levels := []Level{ Debug, Info, Error, Always }
+	levels := []Level{Debug, Info, Error, Always}
 
-    for i := range levels {
-        SetLevel(0, levels[i])
+	for i := range levels {
+		SetLevel(0, levels[i])
 
-        if GetLevel(0) != levels[i] {
-            t.Errorf("Actual: %d, Expected: %d\n", GetLevel(0), levels[i])
-        }
-    }
+		if GetLevel(0) != levels[i] {
+			t.Errorf("Actual: %d, Expected: %d\n", GetLevel(0), levels[i])
+		}
+	}
 }
 
 func TestLoggerPrintlnDebug(t *testing.T) {
-    levels := []Level{ Debug, Info, Error, Always }
-    expected := "[DBG] Debug\n"
+	levels := []Level{Debug, Info, Error, Always}
+	expected := "[DBG] Debug\n"
 
-    for i := range levels {
-        SetLevel(0, levels[i])
-        PrintlnDebug("Debug")
+	for i := range levels {
+		SetLevel(0, levels[i])
+		PrintlnDebug("Debug")
 
-        if GetLevel(0) > Debug && output.String() != "" {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
-        } else if GetLevel(0) <= Debug && output.String() != expected {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
-        }
+		if GetLevel(0) > Debug && output.String() != "" {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
+		} else if GetLevel(0) <= Debug && output.String() != expected {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
+		}
 
-        output.Reset()
-    }
+		output.Reset()
+	}
 }
 
 func TestLoggerPrintlnInfo(t *testing.T) {
-    levels := []Level{ Debug, Info, Error, Always }
-    expected := "[INF] Info\n"
+	levels := []Level{Debug, Info, Error, Always}
+	expected := "[INF] Info\n"
 
-    for i := range levels {
-        SetLevel(0, levels[i])
-        PrintlnInfo("Info")
+	for i := range levels {
+		SetLevel(0, levels[i])
+		PrintlnInfo("Info")
 
-        if GetLevel(0) > Info && output.String() != "" {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
-        } else if GetLevel(0) <= Info && output.String() != expected {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
-        }
+		if GetLevel(0) > Info && output.String() != "" {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
+		} else if GetLevel(0) <= Info && output.String() != expected {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
+		}
 
-        output.Reset()
-    }
+		output.Reset()
+	}
 }
 
 func TestLoggerPrintlnError(t *testing.T) {
-    levels := []Level{ Debug, Info, Error, Always }
-    expected := "[ERR] Error\n"
+	levels := []Level{Debug, Info, Error, Always}
+	expected := "[ERR] Error\n"
 
-    for i := range levels {
-        SetLevel(0, levels[i])
-        PrintlnError("Error")
+	for i := range levels {
+		SetLevel(0, levels[i])
+		PrintlnError("Error")
 
-        if GetLevel(0) > Error && output.String() != "" {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
-        } else if GetLevel(0) <= Error && output.String() != expected {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
-        }
+		if GetLevel(0) > Error && output.String() != "" {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), "")
+		} else if GetLevel(0) <= Error && output.String() != expected {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
+		}
 
-        output.Reset()
-    }
+		output.Reset()
+	}
 }
 
 func TestLoggerPrintlnAlways(t *testing.T) {
-    levels := []Level{ Debug, Info, Error, Always }
-    expected := "[ALW] Always\n"
+	levels := []Level{Debug, Info, Error, Always}
+	expected := "[ALW] Always\n"
 
-    for i := range levels {
-        SetLevel(0, levels[i])
-        PrintlnAlways("Always")
+	for i := range levels {
+		SetLevel(0, levels[i])
+		PrintlnAlways("Always")
 
-        if output.String() != expected {
-            t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
-        }
+		if output.String() != expected {
+			t.Errorf("Actual: %s, Expected: %s\n", output.String(), expected)
+		}
 
-        output.Reset()
-    }
+		output.Reset()
+	}
+}
+
+func TestLoggerPrintlnTime(t *testing.T) {
+	defer func() {
+		var elapsedMs float64 = 0.
+		n, err := fmt.Sscanf(output.String(), "[ALW] (exec time: %fms) Waited for 10ms", &elapsedMs)
+
+		if n != 1 || err != nil || elapsedMs < 10. {
+			t.Errorf("Actual: %s %d %s\n", output.String(), n, err)
+		}
+		output.Reset()
+	}()
+
+	defer PrintlnTime(Always, time.Time{}, "Waited for 10ms")()
+	time.Sleep(time.Millisecond * 10)
 }
